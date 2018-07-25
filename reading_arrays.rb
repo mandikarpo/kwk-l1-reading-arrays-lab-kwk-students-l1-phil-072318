07d7ab57bbf261b7a6e5438f292f303770146223
Mandi Karpo <mandi.karpo@gmail.com>
2018-07-25 14:20:16 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -26,8 +26,10 @@ end
 first_student_by_method
 
 def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
+  puts STUDENT_NAMES.last 
 end
+
+last_student_by_method
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
