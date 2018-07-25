34102607d2459aa6e92bcdef93baf58da004bc6f
Mandi Karpo <mandi.karpo@gmail.com>
2018-07-25 14:21:13 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -23,14 +23,14 @@ def first_student_by_method
   puts STUDENT_NAMES.first 
 end
 
-first_student_by_method
 
 def last_student_by_method
   puts STUDENT_NAMES.last 
 end
 
-last_student_by_method
 
 def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
-end
+  puts STUDENT_NAMES[0,1,2]
+end 
+
+first_second_and_third_students
